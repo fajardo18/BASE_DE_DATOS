c2baba9ca8f4efe072017cc16d87d0a3b438abad
Angelica Fajardo <fajardo1806@gmail.com>
2023-08-09 00:03:27 -0500

    correccion proyecto integrador

---
+++ DB-PROYECTO/DB-Proyecto2.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Categoria(
+	ID INTERGER PRIMARY KEY NOT NULL,
+	NOMBRE TEXT
+);
+
+CREATE TABLE Producto (
+ 	ID INT PRIMARY KEY NOT NULL,
+    Nombre TEXT,
+    Marca TEXT,
+    Categoria_id INT,
+    Precio_unitario NUMERIC
+   );
+
+CREATE TABLE Sucursal (
+    ID INT PRIMARY KEY NOT NULL,
+    Nombre TEXT,
+    Direccion TEXT
+);
+
+CREATE TABLE Stock (
+    ID Serial PRIMARY KEY NOT NULL,
+    Sucursal_id INT,
+    Producto_id INT,
+    Cantidad INT,
+    UNIQUE (sucursal_id, producto_id)
+    );
+
+CREATE TABLE Cliente(
+	ID INT PRIMARY KEY NOT NULL,
+	Nombre TEXT,
+	Telefono INT
+);
+
+CREATE TABLE Orden (
+    ID INT PRIMARY KEY NOT NULL,
+    Cliente_id INT,
+    Sucursal_id INT,
+    Fecha DATE,
+    Total NUMERIC
+    );
+
+CREATE TABLE Item (
+    ID INT PRIMARY KEY NOT NULL,
+    Orden_id INT,
+    Producto_id INT,
+    Cantidad INT,
+    Monto_venta NUMERIC
+    );
